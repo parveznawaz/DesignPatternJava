b9b2941c9e9337424fdd82d318e5188d0343fa5b
Parvez Nawaz <ParvezN@gs1ca.org>
2020-01-24 13:43:21 -0500

    Factory method added

---
+++ src/creational/factory/example1/Rectangle.java
@@ -0,0 +1,8 @@
+package creational.factory.example1;
+
+public class Rectangle implements Shape {
+    @Override
+    public void draw() {
+        System.out.println("Inside rectangle::draw method");
+    }
+}
---
+++ src/creational/factory/example1/Shape.java
@@ -0,0 +1,5 @@
+package creational.factory.example1;
+
+public interface Shape {
+    void draw();
+}
